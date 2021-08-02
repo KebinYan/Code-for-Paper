3e9d536ddada81927637ca8b86e197d22d7de79a
KebinYan <54713787+KebinYan@users.noreply.github.com>
2021-08-02 15:15:36 -0400

    Add files via upload

--- Universal R Functions/Examining Time Difference.R
+++ Universal R Functions/Examining Time Difference.R
@@ -1,16 +1,16 @@
-# calculate the time difference between each gyroscope timestamp and the closest accelerometer timestamp
+# calculate time difference between accelerometer timestamp and the closest gyroscope timestamp
 CalculateTimeDifference <- function(accData, gyroData){
-  time_difference <- as.data.frame(cbind(gyroData$timestamp, rep(NA,nrow(gyroData)), rep(NA,nrow(gyroData))))
-  colnames(time_difference) <- c("gyro_timestamp","acc_timestamp","time_difference")
+  time_difference <- as.data.frame(cbind(accData$timestamp, rep(NA,nrow(accData)), rep(NA,nrow(accData))))
+  colnames(time_difference) <- c("acc_timestamp","closest_gyro_timestamp","time_difference")
   
-  for (i in 1:nrow(gyroData)){
+  for (i in 1:nrow(accData)){
     temp_diff <- NA
-    temp_diff <- as.data.frame(rep(NA, nrow(accData)))
-    for(j in 1:nrow(accData)){
-      temp_diff[j,1] <- abs(gyroData$timestamp[i]-accData$timestamp[j])
+    temp_diff <- as.data.frame(rep(NA, nrow(gyroData)))
+    for(j in 1:nrow(gyroData)){
+      temp_diff[j,1] <- abs(accData$timestamp[i]-gyroData$timestamp[j])
     }
     time_difference[i,3] <- min(temp_diff)
-    time_difference[i,2] <- accData$timestamp[which.min(unlist(temp_diff))]
+    time_difference[i,2] <- gyroData$timestamp[which.min(unlist(temp_diff))]
   }
   
   return(time_difference)
